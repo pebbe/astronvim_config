d046543d5db8a1a09d0edb2967d8ee1de77fd39a
Peter Kleiweg <kleiweg@ziggo.nl>
2023-04-18 15:39:09 +0200

    update na tutorial op https://www.youtube.com/watch?v=GEHPiZ10gOk

--- mappings.lua
+++ mappings.lua
@@ -20,9 +20,13 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    ["<leader>a"] = { "<cmd>echo 'Moi, Grunn!'<cr>", desc = "Zeg Moi!" },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  v = {
+
   },
 }
